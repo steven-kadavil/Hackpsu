b5687bb53254386fc40c05a18d0a3ed7bfacec0a
andytieu <104803764+andytieu@users.noreply.github.com>
2024-10-12 16:24:34 -0400

    css + conversation input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./App.module.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <label className={styles.conversation_input_label}>Talk to our AI to build your Career Roadmap</label>
+    <div className={styles.top_section}>
+      <input className={styles.conversation_input} placeholder="Enter your Career Goals"></input>
+      <button className={styles.conversation_button}></button>
+    </div>
     </>
   )
 }
